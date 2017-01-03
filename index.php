9a994e7d104b73c8e4dcc67f25eeebe0b7008716
Thomas <ThomasJ@moh.gov.jm>
2017-01-03 17:56:38 -0500

    Basic Frontend Development project for Free Code Camp

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="device-width, initial-scale=1">
+
+	<title>A tribute to Nathaniel Kleitman</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<style type="text/css">
+		
+		#bio_title{
+			font-family: 'Lobster', 'cursive';
+			background: rgba(0,0,0,0.7);
+			z-index: 10;
+			position: relative;
+			top: 500px;
+			width: 100%;
+
+		}
+
+		#title_background{
+			align-content: center;
+			z-index: 1;
+			width: 100%;
+			position: absolute;
+		}
+
+		#information{
+			z-index: 11;
+		}
+
+		h1,h3{
+
+			color: white;
+		}
+
+		
+		p{
+			font-family: 'cursive';
+		}
+
+		body{
+
+		}
+
+	</style>
+
+
+<body>
+
+<div id="wrapper">
+	<div id="header"  class="container-fluid">
+
+		<div id="title_background">
+		<img class="img-responsive" src="pics/kleitman_image.png" alt="A picture of Nathaniel Kleitman">
+		</div>
+
+		<div id="bio_title" class="text-center">
+		<h1>Dr Nathaniel Kleitman (Apr 26 1895 - Aug 13 1999)</h1>
+
+		<h3>The Father of Modern Sleep Research</h3>
+		</div>
+	</div>
+	
+	<div id="information" class="container-fluid">
+		
+		<div class="col-md-4"><p>#Intro</p></div>
+
+		<div class="col-md-4"><p>#History</p></div>
+
+		<div class="col-md-4"><p>#Key Discoveries</p></div>
+
+		<div class="col-md-12"><p>#More information</p>
+	</div>
+</div>
+
+ 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
